78fc6300de4ab4b17640923dba7ff2c992dccb23
Katka <vocseikati@gmail.com>
2021-12-08 15:05:52 +0100

    Thymeleaf exercises added

---
+++ Airport/src/main/java/hu/webuni/airport/controllers/AirportTLController.java
@@ -0,0 +1,37 @@
+package hu.webuni.airport.controllers;
+
+import hu.webuni.airport.dtos.AirportDto;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AirportTLController {
+
+  private List<AirportDto> allAirports = new ArrayList<>();
+
+  {
+    allAirports.add(new AirportDto(1, "Ferenc Liszt Airport", "BUD"));
+  }
+
+  @GetMapping("/")
+  public String home() {
+    return "index";
+  }
+
+  @GetMapping("/airports")
+  public String listAirports(Map<String, Object> model) {
+    model.put("airports", allAirports);
+    model.put("newAirport", new AirportDto());
+    return "airports";
+  }
+
+  @PostMapping("/airports")
+  public String addAirport(AirportDto airport) {
+    allAirports.add(airport);
+    return "redirect:airports";
+  }
+}
