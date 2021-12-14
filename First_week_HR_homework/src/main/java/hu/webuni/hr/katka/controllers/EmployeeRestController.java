d71233eb2f088791b7adc6bd9943cdb60a8e31ff
Katka <vocseikati@gmail.com>
2021-12-14 01:12:36 +0100

    Put and Delete methods added to RestApi

--- First_week_HR_homework/src/main/java/hu/webuni/hr/katka/controllers/EmployeeRestController.java
+++ First_week_HR_homework/src/main/java/hu/webuni/hr/katka/controllers/EmployeeRestController.java
@@ -1,7 +1,8 @@
 package hu.webuni.hr.katka.controllers;
 
 import hu.webuni.hr.katka.dtos.EmployeeDto;
-import hu.webuni.hr.katka.exceptions.MyException;
+import hu.webuni.hr.katka.exceptions.NotFoundException;
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,9 +10,11 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,8 +35,8 @@ public class EmployeeRestController {
   private Map<Long, EmployeeDto> employees = new HashMap<>();
 
   {
-    employees.put(1L, employeeList.get(1));
-    employees.put(2L, employeeList.get(0));
+    employees.put(1L, employeeList.get(0));
+    employees.put(2L, employeeList.get(1));
   }
 
 
@@ -42,7 +45,7 @@ public class EmployeeRestController {
     return ResponseEntity.ok(employees);
   }
 
-  @GetMapping("/list")
+  @GetMapping("/listmap")
   public ResponseEntity<Map<String, List<EmployeeDto>>> listAllEmployees() {
     Map<String, List<EmployeeDto>> listMap = new HashMap<>();
     listMap.put("employees", employeeList);
@@ -52,13 +55,12 @@ public class EmployeeRestController {
   @GetMapping("/{id}")
   public ResponseEntity<?> getEmployeeById(@PathVariable Long id) {
     validateFields(id, "Id cannot be null!");
-    for (Map.Entry<Long, EmployeeDto> entry : employees.entrySet()) {
-      if (entry.getValue().getId().equals(id)) {
-        return ResponseEntity.ok(entry.getValue());
-      }
+    EmployeeDto employee = employees.get(id);
+    if (employee != null) {
+      return ResponseEntity.ok(employee);
     }
     String error =
-        new MyException("There is no employee with the provided id.").getMessage();
+        new NotFoundException("There is no employee with the provided id.").getMessage();
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
   }
 
@@ -70,12 +72,50 @@ public class EmployeeRestController {
       validateFields(employee.getPosition(), "Position cannot be null or empty.");
       validateFields(employee.getSalary(), "Salary cannot be null.");
       validateFields(employee.getStartOfWork(), "Date cannot be null.");
-    } catch (IllegalArgumentException ex){
+    } catch (IllegalArgumentException ex) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
     employee.setId((long) (employees.size() + 1));
     employees.put((long) (employees.size() + 1), employee);
-    return ResponseEntity.ok(employee);
+    URI location = URI.create(String.format("api/employees/%s", employee.getId()));
+    return ResponseEntity.created(location).body(employee);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<?> modifyEmployeeById(@PathVariable Long id,
+                                              @RequestBody EmployeeDto employee) {
+    validateFields(employee, "Employee cannot be null.");
+    validateFields(id, "Id cannot be null!");
+    if (!employees.containsKey(id)){
+      String error = new NotFoundException("There is no employee with the provided id.").getMessage();
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+    EmployeeDto originalEmployee = employees.get(id);
+    if (employee.getName() != null){
+      originalEmployee.setName(employee.getName());
+    }
+    if (employee.getPosition() != null){
+      originalEmployee.setPosition(employee.getPosition());
+    }
+    if (employee.getSalary() != null){
+      originalEmployee.setSalary(employee.getSalary());
+    }
+    if (employee.getStartOfWork() != null){
+      originalEmployee.setStartOfWork(employee.getStartOfWork());
+    }
+    return ResponseEntity.ok(originalEmployee);
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<?> deleteEmployeeById(@PathVariable Long id){
+    validateFields(id, "Id cannot be null!");
+    if (!employees.containsKey(id)){
+      String error = new NotFoundException("There is no employee with the provided id.").getMessage();
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+    EmployeeDto deletedEmployee = employees.get(id);
+    employees.remove(id);
+    return ResponseEntity.ok(deletedEmployee);
   }
 
   private void validateFields(Object o, String message) {
